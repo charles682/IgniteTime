ddc07016415e487e8b77280e1582fc5f3d663e9c
Charles Melo <melocharles220@gmail.com>
2024-08-15 20:16:32 -0300

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,12 @@
+import 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
+
+type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends ThemeType {}
+}  
